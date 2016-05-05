7d0dea21f96c65d597703911309af01a98eb1527
Chui <lordbabypanda@gmail.com>
2016-05-05 18:05:23 -0500

    JavaScript Notes.

---
+++ JavaScript.js
@@ -0,0 +1,87 @@
+Code Acadmey
+-----------------------
+"Paul"
+"Paul".length // Length of your name
+3+4 // adding numbers
+10 * 2 // Multiple
+10/2 // Divide
+
+// // Javascript comments like this! FREAKING AWESOME!
+
+/*
+What can we use JavaScript for?
+ 	make websites respond to user interaction
+ 	build apps and games (e.g. blackjack)
+ 	access information on the Internet (e.g. find out the top trending words on Twitter by topic)
+ 	organize and present data (e.g. automate spreadsheet work; data visualization)
+
+
+confirm("I feel awesome); // Letting the user confirm things.
+
+Prompt, asks user questions and for their input
+
+console.log() // Whatever is in the parenthese will log to console.
+
+Data types
+
+strings (e.g. "dogs go woof!")
+numbers (e.g. 4, 10)
+booleans (e.g. false, 5 > 4)
+
+Manipulating numbers & strings
+
+comparisons (e.g. >, <=)
+modulo (e.g. %)
+string length (e.g. "Emily".length;)
+substrings (e.g. "hi".substring(0, 1);)
+
+console.log( ) 
+Prints into the console whatever we put in the parentheses.
+
+var divideByThree = function (number) 
+{
+    var val = number / 3;
+    console.log(val);
+};
+
+// On line 12, we call the function by name
+// Here, it is called 'dividebythree'
+// We tell the computer what the number input is (i.e. 6)
+// The computer then runs the code inside the function!
+divideByThree(6);
+
+var = variable
+
+Function is a function name () whatever you pass in this parameter, 
+must be enter with correct data type;
+
+Functions may have two parameters
+
+
+return keyword = back value that comes out of the function
+
+learned for loops, already know it, I'm good! 
+for(var i= 1; i < 100; ++i )
+{
+	console.log(i);
+}
+
+=== is equal value and equal type
+== equal value
+
+arrays = var names = ["panda", "cookies", "butt", "zorthi", "pandalord"];
+
+while loops
+
+var bool = true;
+
+while(bool)
+{
+	console.log("Blah me")
+	bool = false;
+}
+*/
+
+var country = "America";
+
+console.log(country.substring(0,3)); // Takes elements of the string you desire
